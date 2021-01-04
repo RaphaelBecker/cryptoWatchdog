fe061924b9fb9f6388bdd1c6b1ec037f134c375c
Waleed <waleed.khalid@tum.de>
2021-01-04 21:49:00 +0100

    Added auto updating for data retreival

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 from data_aquisition.requester import get_binance_data
 from data_processing.data_processor import data_cruncher
+import time
 
 
 def main():
-    data = get_binance_data()
-    data_cruncher(data)
+    while True:
+        data = get_binance_data()
+        data_cruncher(data)
+        update_time = 5;
+        # Updates data every update_time seconds
+        time.sleep(update_time)
+
 
 
 if __name__ == "__main__":

c0e25f83769a26d55bfd203729d30a16e7d3fdcf
Waleed <waleed.khalid@tum.de>
2021-01-05 21:19:53 +0100

    Extracted coin name, price, volume and date data Saved data to csv In case the csv already existed, appended data to csv All the data is saved in the directory ./data/coin_price_data/*.csv
    Closes #3
    Closes #4

--- data_processing/data_processor.py
+++ data_processing/data_processor.py
@@ -1,8 +1,53 @@
+import pandas as pd
+import os.path
+
 
 def data_cruncher(raw_dataframe):
-    print(raw_dataframe)
+    """
+    :param raw_dataframe:
+    :return: new dataframe for each coin
+    """
+    print(raw_dataframe.head())
+    for coin in raw_dataframe.iterrows():
+        # Gathering data for each specific coin
+        coin_name, coin_price, coin_volume, date_time = get_key_coin_data(coin)
+
+        # Generate dataframe for particular time instance
+        coin_data = pd.DataFrame({"Coin Name": coin_name, "Price": coin_price,
+                                  "Volume": coin_volume, "Date/Time": date_time},
+                                 index=[0])
+
+        # Save data to csv file
+        save_data_to_file(coin_name, coin_data)
+
     # TODO: Filter relevant coins
     # TODO: Map coin abbreviation to coin full name
-    # TODO: Extract individual coin price and time stamp
-    # TODO: Save in coins own csv file for logging purpose
     # TODO: Check how heroku works
+
+
+def get_key_coin_data(coin):
+    """
+    Extract important data from coin
+    :param coin:
+    :return:
+    """
+    coin_name = coin[1].base
+    coin_price = coin[1].price
+    coin_volume = coin[1].volume
+    date_time = coin[1].Time
+
+    return coin_name, coin_price, coin_volume, date_time
+
+
+def save_data_to_file(coin_name, data: pd.DataFrame):
+    """
+    :param coin_name:
+    :param data:
+    Checks if csv file exists. If it doesnt, creates
+    file otherwise append to file
+    """
+    file_name = "./data/coin_price_data/" + coin_name + "_data.csv"
+    if os.path.isfile(file_name):
+        data.to_csv(file_name, mode='a', header=False)
+    else:
+        data.to_csv(file_name)

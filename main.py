1d7821af2623ee199beeb5319c28585a962c6e18
Waleed <waleed.khalid@tum.de>
2021-01-13 22:41:44 +0100

    Saved API data in env var and added bot initialization msgs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from data_aquisition.requester import get_binance_data
 from data_processing.data_processor import data_cruncher
+from telegram_bot.notificator import bot_ready
 
 import time
 
@@ -8,18 +9,20 @@ import time
 def main():
     """
     Program is going to run unless interrupted manually
-    :return: Nothing
+    :return: void
     """
     i = 0
     while True:
+        bot_ready("-------Analyzing market data-------")
         update_time = 10
         i += 1
         print("Running...", i)
         data = get_binance_data()
         data_cruncher(data)
         # Updates data every update_time seconds
-        time.sleep(update_time)
+        bot_ready("-----------Task finished-----------")
 
+        time.sleep(update_time)
 
 if __name__ == "__main__":
     main()
--- telegram_bot/notificator.py
+++ telegram_bot/notificator.py
@@ -1,19 +1,39 @@
 import requests
-
+import os
 
 def post_message_to_chat_group(message_string):
-    base_url = 'https://api.telegram.org/bot1597486965:AAGIjRUzD9NTJfaeOgjvmSDNyMAL6NQ7O5c/sendMessage?chat_id=-416548268&text="{}"'.format(message_string)
+    """
+    Posts message to telegram group
+    :param message_string:
+    :return:
+    """
+    # TODO: add API key to env vars before release
+    api_key_url = os.environ.get('API_KEY_URL')
+    base_url = api_key_url + '"{}"'.format(message_string)
     requests.get(base_url)
 
 
 def alert_rsi(coin_name, rsi, date_time):
+    """
+    :param coin_name:
+    :param rsi: RSI-time_span value
+    :param date_time: current date/time
+    :return: void - shoots a message to the telegram group incase something interesting happens
+    """
     rsi_upper_bound = 70
     rsi_lower_bound = 30
     if rsi > rsi_upper_bound:
-        message = coin_name + " is overbought. Time to sell! RSI = " + str(rsi)
+        message = coin_name + ' is overbought. Time to sell! RSI = "{:.2f}"'.format(rsi)
         post_message_to_chat_group(message)
-    elif rsi > rsi_lower_bound:
-        message = coin_name + " is oversold. Time to buy! RSI = " + str(rsi)
+    elif rsi < rsi_lower_bound:
+        message = coin_name + ' is oversold. Time to buy! RSI = "{:.2f}"'.format(rsi)
         post_message_to_chat_group(message)
     else:
         pass
+
+def bot_ready(message):
+    """
+    send an initialization message to group
+    :return:
+    """
+    post_message_to_chat_group(message)

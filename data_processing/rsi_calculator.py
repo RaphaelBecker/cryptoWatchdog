81d4b1b36974176a363a204cdfa5a37a3e60d71e
Waleed <waleed.khalid@tum.de>
2021-05-16 17:06:42 +0200

    New rsi_calculator function incorporated and coin filtering done in main function

--- data_processing/rsi_calculator.py
+++ data_processing/rsi_calculator.py
@@ -2,46 +2,51 @@ import warnings
 
 import numpy as np
 from math import nan
+from data_aquisition.database_requester import get_every_nth_price_as_dataframe
 
+def rsi_calculator(coin, time_space=14):
+    print(coin)
+    # coin_price_data = get_every_nth_price_as_dataframe(1, coin)
 
-def rsi_calculator(coin_data, coin_price: float, time_span=14):
-    """
-    :param coin_data: stored historic data
-    :param coin_price: The current price of the coin
-    :param time_span: Time for calculating RSI
-    :return: rsi, rs, average_gain, average_loss, loss, gain
-    """
-    # Calculate loss or gain
-    last_price = float(coin_data["Price"].tail(1))
-    change = coin_price - last_price
-    if change >= 0:
-        # Case for loss
-        gain = abs(change)
-        loss = 0
-    elif change < 0:
-        # Case for win
-        loss = abs(change)
-        gain = 0
-    else:
-        raise ValueError("Non Numeric Operations took place")
-
-    # Calculating average wins and losses
-    if len(coin_data.index) > time_span - 1:
-        historic_gains = coin_data["Gain"].tail(time_span - 1) \
-            .tolist()
-        historic_gains.append(gain)
-        historic_losses = coin_data["Loss"].tail(time_span - 1) \
-            .tolist()
-        historic_losses.append(loss)
-        average_gain = np.mean(historic_gains)
-        average_loss = np.mean(historic_losses)
-        rs = average_gain / average_loss
-        rsi = 100 - 100 / (1 + rs)
-    else:
-        average_gain = nan
-        average_loss = nan
-        rs = nan
-        rsi = nan
-        warnings.warn('price data needs to be >"{}"'.format(time_span))
-
-    return rsi, rs, average_gain, average_loss, loss, gain
+"""Depricated RSI calculator code which used csv files"""
+# def rsi_calculator(time_span=14):
+#     """
+#     :param coin_data: stored historic data
+#     :param coin_price: The current price of the coin
+#     :param time_span: Time for calculating RSI
+#     :return: rsi, rs, average_gain, average_loss, loss, gain
+#     """
+#     # Calculate loss or gain
+#     last_price = float(coin_data["Price"].tail(1))
+#     change = coin_price - last_price
+#     if change >= 0:
+#         # Case for loss
+#         gain = abs(change)
+#         loss = 0
+#     elif change < 0:
+#         # Case for win
+#         loss = abs(change)
+#         gain = 0
+#     else:
+#         raise ValueError("Non Numeric Operations took place")
+#
+#     # Calculating average wins and losses
+#     if len(coin_data.index) > time_span - 1:
+#         historic_gains = coin_data["Gain"].tail(time_span - 1) \
+#             .tolist()
+#         historic_gains.append(gain)
+#         historic_losses = coin_data["Loss"].tail(time_span - 1) \
+#             .tolist()
+#         historic_losses.append(loss)
+#         average_gain = np.mean(historic_gains)
+#         average_loss = np.mean(historic_losses)
+#         rs = average_gain / average_loss
+#         rsi = 100 - 100 / (1 + rs)
+#     else:
+#         average_gain = nan
+#         average_loss = nan
+#         rs = nan
+#         rsi = nan
+#         warnings.warn('price data needs to be >"{}"'.format(time_span))
+#
+#     return rsi, rs, average_gain, average_loss, loss, gain
--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 from data_aquisition.requester import get_binance_data
 from data_processing.data_processor import data_cruncher
 from telegram_bot.notificator import bot_ready
-
-import time
+from data_processing.rsi_calculator import rsi_calculator
 from datetime import date
+import csv
+import os
+import time
 
 def main():
     """
@@ -13,16 +15,36 @@ def main():
     """
     i = 0
     while True:
+        # TODO: Undo this after testing
         bot_ready('---Analyzing market data on "{}"'.format(date.today()) + "---")
-        update_time = 60*60*6
+        # TODO: change sleep time after testing
+        update_time = 10 #60*60*6
         i += 1
         print("Running...", i)
-        data = get_binance_data()
-        data_cruncher(data)
+        with open('./data_processing/resources/selectedCurrencies.csv', newline='') as f:
+            reader = csv.reader(f)
+            coins = list(reader)
+            coins = coins[0]
+        cwd = os.path.abspath(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data_processing', 'resources'
+                         , 'nameSymbolDictionary.csv'))
+
+        # To convert from coin symbol to coin name as that is how they are stored in the database
+        with open('./data_processing/resources/nameSymbolDictionary.csv') as file:
+            abv_to_coin_name_dict = csv.reader(file)
+            abv_to_coin_name_dict = dict(abv_to_coin_name_dict)
+
+        for coin in coins:
+            rsi_calculator(abv_to_coin_name_dict[coin])
+        # No longer needed as data is being fetched from the database.
+        # data = get_binance_data()
+        # data_cruncher(data)
+        # RSI Calculation
+
         # Updates data every update_time seconds
-        bot_ready("-----------Task finished-----------")
-
+        # TODO: Undo this after testing
+        # bot_ready("-----------Task finished-----------")
         time.sleep(update_time)
-
+#
 if __name__ == "__main__":
     main()

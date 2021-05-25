d2e2fb43c934364d2397091318747654db18f761
Raphael Becker <ge38xed@tum.de>
2021-05-25 19:51:59 +0200

    deleted path to telegram

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -1,30 +1,31 @@
 # #%% <-  Delete # for scientific mode!
 # https://coderzcolumn.com/tutorials/data-science/candlestick-chart-in-python-mplfinance-plotly-bokeh
+# https://www.youtube.com/watch?v=pkk5U-8Vl7A
 #%%
-import cufflinks as cf
+
 import pandas as pd
-import plotly.graph_objects as go
+import mplfinance as mpf
+import plotly as py
+import requests
+import io
+# test: displays images
+
+
+print(py.__version__)
 
 apple_df = pd.read_csv('visualization/AAPL.csv', index_col=0, parse_dates=True)
-dt_range = pd.date_range(start="2020-03-01", end="2020-03-31")
-apple_df = apple_df[apple_df.index.isin(dt_range)]
+apple_df.info()
+# dt_range = pd.date_range(start="2020-03-01", end="2020-03-31")
+# apple_df = apple_df[apple_df.index.isin(dt_range)]
 print(apple_df.head())
-
-qf = cf.QuantFig(apple_df, title='Apple Quant Figure', legend='top', name='GS')
-
-qf.add_bollinger_bands(periods=20, boll_std=2, colors=['magenta', 'grey'], fill=True)
-qf.add_sma([10, 20], width=2, color=['green', 'lightgreen'],legendgroup=True)
-qf.add_rsi(periods=20, color='java')
+# apple_df = apple_df.set_index('Date')
+imageBuffer = io.BytesIO()
+mpf.plot(apple_df, type='candle', volume=True, mav=(20,40), savefig=imageBuffer)
+mpf.savefig(imageBuffer, format='png')
 
 
-print(type(qf))
-plot = qf.figure()
-print(type(plot))
-plot.show(renderer="svg")
-fig = go.Figure()
 
-qf.iplot()
-# qf.iplot(asImage=True)
+
 
 
 
